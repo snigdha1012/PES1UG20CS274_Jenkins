7f9085a9b392462835dc6e3ef2bac55ec53c889d
snigdha1012 <76674154+snigdha1012@users.noreply.github.com>
2023-02-15 10:31:51 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "CC lab"
+  cout << "CC lab";
   return 0;
 }
